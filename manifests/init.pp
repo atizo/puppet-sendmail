7adec2de0e1ad3813de937fc1c135a625b326638
Marcel Haerry <haerry@puzzle.ch>
2008-05-06 16:42:04 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# sendmail module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "sendmail": }
+class sendmail {}
