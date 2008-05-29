731624875e404e8012f77409acb98c3a6604b7de
Simon Josi <sjosi@pw-sjosi.puzzle.itc>
2008-05-29 18:39:42 +0200

    added puzzle copyright notice

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,18 @@
-#######################################
-# sendmail module
-# Puzzle ITC - haerry+puppet(at)puzzle.ch
-# GPLv3
 #
-# manages sendmail stuff
-#######################################
-
+# sendmail module
+#
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 # modules_dir { "sendmail": }
+
 class sendmail {
     include sendmail::base
 
