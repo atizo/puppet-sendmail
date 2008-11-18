6b820ab253f3a3383eeed2421a2f1b2b0d2d2ea6
Marcel Haerry <haerry@puzzle.ch>
2008-11-18 15:48:48 +0100

    fixed munin plugins

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,11 +1,17 @@
 # manifests/munin.pp
 
 class sendmail::munin {
-    munin::plugin{'sendmail': }
+    munin::plugin{ [ 'sendmail_mailqueue', 'sendmail_mailstats', 'sendmail_mailtraffic' ]: }
 }
 
 class sendmail::munin::disable inherits sendmail::munin {
-    Munin::Plugin['sendmail']{
+    Munin::Plugin['sendmail_mailstats']{
+         ensure => 'absent' 
+    }
+    Munin::Plugin['sendmail_mailtraffic']{
+         ensure => 'absent' 
+    }
+    Munin::Plugin['sendmail_mailqueue']{
          ensure => 'absent' 
     }
 }
